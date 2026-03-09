ae4f3aee3e11c0418259d6995367a904a05bc795
Louis Melchiorre <ltm893@icloud.com>
2026-03-09 14:36:40 -0400

    update setup

--- setup.sh
+++ setup.sh
@@ -3,3 +3,6 @@ echo "Setting up git templates..."
 cp -r .git-templates ~/.git-templates
 git config --global init.templateDir ~/.git-templates
 echo "✅ Done"
+
+# sync .zshrc
+ln -sf "$HOME/Dev/projects/dotfiles/.zshrc" "$HOME/.zshrc"
